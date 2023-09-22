c183f0dec5ad80f8bbd5f5db7ae5b8a88a397a05
thedavid54 <thedavid5432@gmail.com>
2023-09-22 08:40:21 +0800

    init

---
+++ src/main/java/the/david/randomdungeon/RandomDungeon.java
@@ -0,0 +1,17 @@
+package the.david.randomdungeon;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class RandomDungeon extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
