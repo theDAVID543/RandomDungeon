859a8139b859d7495ff4ec128842f3a0bf4ef10b
thedavid54 <thedavid5443@gmail.com>
2023-09-25 15:29:02 +0800

    update

--- src/main/java/the/david/randomdungeon/dungeon/holder/Room.java
+++ src/main/java/the/david/randomdungeon/dungeon/holder/Room.java
@@ -12,6 +12,41 @@ public class Room {
     private String roomName;
     private Location pos1;
     private Location pos2;
+    private Boolean doorEast;
+    private Boolean doorWest;
+    private Boolean doorNorth;
+    private Boolean doorSouth;
+    private Boolean canRotate;
+
+    public void setDoorDirection(String direction){
+        if(direction.toUpperCase().contains("E")){
+            doorEast = true;
+        }
+        if(direction.toUpperCase().contains("W")){
+            doorWest = true;
+        }
+        if(direction.toUpperCase().contains("N")){
+            doorNorth = true;
+        }
+        if(direction.toUpperCase().contains("S")){
+            doorSouth = true;
+        }
+    }
+    public Boolean getDoorEast(){
+        return doorEast;
+    }
+    public Boolean getDoorWest(){
+        return doorWest;
+    }
+    public Boolean getDoorNorth(){
+        return doorNorth;
+    }
+    public Boolean getDoorSouth(){
+        return doorSouth;
+    }
+    public Boolean getCanRotate(){
+        return canRotate;
+    }
 
     public void setPos1(Location location){
         pos1 = location;
